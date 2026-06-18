2b5e1d27a84dd935052e9a553dec0abb3b767a35
David Lim <ddavidd.lim@gmail.com>
2026-06-18 16:10:28 -0700

    feat: added supabase client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,26 @@
 import { SimpleEditor } from '@/components/tiptap-templates/simple/simple-editor'
+import { useEffect, useState } from 'react'
+import { supabase } from './clients/supabase'
+
+
+
 
 export default function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    async function getNotes() {
+      const { data: notes } = await supabase.from('notes').select('*');
+
+      if (notes) {
+        setNotes(notes)
+      }
+    }
+
+    getNotes()
+  }, [])
+
+  console.log(notes)
+
   return <SimpleEditor />
 }
