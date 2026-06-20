b9178f08c60ee78951675c4826812e68219f23c7
David Lim <ddavidd.lim@gmail.com>
2026-06-20 13:15:46 -0700

    chore: modifying eslint rules

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,4 @@
 import { SimpleEditor } from '@/components/tiptap-templates/simple/simple-editor'
-import { useEffect, useState } from 'react'
-import { supabase } from './clients/supabase'
-
-
-
 
 export default function App() {
 
--- client/src/components/tiptap-templates/simple/simple-editor.tsx
+++ client/src/components/tiptap-templates/simple/simple-editor.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/set-state-in-effect */
+/* eslint-disable react-hooks/refs */
 "use client"
 
 import { useEffect, useRef, useState } from "react"
